5989e2a73bffd68faaeb931cdd7e389cf7c2e817
monkeyman192 <monkey_man_192@yahoo.com.au>
2024-12-11 23:26:43 +1100

    Update for 16706643

--- libMBIN/Source/NMS/GameComponents/GcCostGameMode.cs
+++ libMBIN/Source/NMS/GameComponents/GcCostGameMode.cs
@@ -2,14 +2,16 @@ using libMBIN.NMS.GameComponents;
 
 namespace libMBIN.NMS.GameComponents
 {
-    [NMS(GUID = 0x10AA5823518E256E, NameHash = 0x524F0ABF)]
+    [NMS(GUID = 0xD3FD505117D654A6, NameHash = 0x524F0ABF)]
     public class GcCostGameMode : NMSTemplate
     {
         [NMS(Index = 2)]
         /* 0x00 */ public NMSString0x20A CostStringCantAfford;
         [NMS(Index = 1)]
         /* 0x20 */ public GcGameMode Mode;
+        [NMS(Index = 3)]
+        /* 0x24 */ public int SpecificSeasonIndex;
         [NMS(Index = 0)]
-        /* 0x24 */ public bool InvertMode;
+        /* 0x28 */ public bool InvertMode;
     }
 }
