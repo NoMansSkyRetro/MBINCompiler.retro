b0f1ecbdd43065e7439f70cfaa59ccaf801a17ad
monkeyman192 <monkey_man_192@yahoo.com.au>
2025-06-18 08:26:51 +1000

    Update for steam build id 18899088

--- libMBIN/Source/NMS/GameComponents/GcInteractionComponentData.cs
+++ libMBIN/Source/NMS/GameComponents/GcInteractionComponentData.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace libMBIN.NMS.GameComponents
 {
-    [NMS(GUID = 0x70CAC62AE17E5438, NameHash = 0xBE246A5)]
+    [NMS(GUID = 0xBB79071744A0C6DE, NameHash = 0xBE246A5)]
     public class GcInteractionComponentData : NMSTemplate
     {
         [NMS(Index = 3)]
@@ -25,26 +25,28 @@ namespace libMBIN.NMS.GameComponents
         /* 0x290 */ public List<TkModelRendererData> EventRenderersAlt;
         [NMS(Index = 23)]
         /* 0x2A0 */ public List<GcInteractionDof> EventRenderersDoF;
+        [NMS(Index = 42)]
+        /* 0x2B0 */ public NMSTemplate InteractionSpecificData;
         [NMS(Index = 26)]
-        /* 0x2B0 */ public List<GcAlienPuzzleMissionOverride> PuzzleMissionOverrideTable;
+        /* 0x2C0 */ public List<GcAlienPuzzleMissionOverride> PuzzleMissionOverrideTable;
         [NMS(Index = 38)]
-        /* 0x2C0 */ public NMSString0x10 StartMissionOnUse;
+        /* 0x2D0 */ public NMSString0x10 StartMissionOnUse;
         [NMS(Index = 13)]
-        /* 0x2D0 */ public NMSString0x10 TriggerAction;
+        /* 0x2E0 */ public NMSString0x10 TriggerAction;
         [NMS(Index = 14)]
-        /* 0x2E0 */ public NMSString0x10 TriggerActionOnPrepare;
+        /* 0x2F0 */ public NMSString0x10 TriggerActionOnPrepare;
         [NMS(Index = 25)]
-        /* 0x2F0 */ public GcInteractionDof DepthOfField;
+        /* 0x300 */ public GcInteractionDof DepthOfField;
         [NMS(Index = 6)]
-        /* 0x304 */ public float AttractDistanceSq;
+        /* 0x314 */ public float AttractDistanceSq;
         [NMS(Index = 11)]
-        /* 0x308 */ public float BlendFromCameraTime;
+        /* 0x318 */ public float BlendFromCameraTime;
         [NMS(Index = 10)]
-        /* 0x30C */ public float BlendToCameraTime;
+        /* 0x31C */ public float BlendToCameraTime;
         [NMS(Index = 16)]
-        /* 0x310 */ public float InteractAngle;
+        /* 0x320 */ public float InteractAngle;
         [NMS(Index = 17)]
-        /* 0x314 */ public float InteractDistance;
+        /* 0x324 */ public float InteractDistance;
         // size: 0x3
         public enum InteractionActionEnum : uint {
             PressButton,
@@ -52,19 +54,19 @@ namespace libMBIN.NMS.GameComponents
             Shoot,
         }
         [NMS(Index = 0)]
-        /* 0x318 */ public InteractionActionEnum InteractionAction;
+        /* 0x328 */ public InteractionActionEnum InteractionAction;
         [NMS(Index = 1)]
-        /* 0x31C */ public GcInteractionType InteractionType;
+        /* 0x32C */ public GcInteractionType InteractionType;
         [NMS(Index = 35)]
-        /* 0x320 */ public float InWorldUIForcedOffset;
+        /* 0x330 */ public float InWorldUIForcedOffset;
         [NMS(Index = 36)]
-        /* 0x324 */ public float InWorldUIForcedOffsetV2;
+        /* 0x334 */ public float InWorldUIForcedOffsetV2;
         [NMS(Index = 33)]
-        /* 0x328 */ public float InWorldUIMinDistOverride;
+        /* 0x338 */ public float InWorldUIMinDistOverride;
         [NMS(Index = 34)]
-        /* 0x32C */ public float InWorldUIMinDistOverrideV2;
+        /* 0x33C */ public float InWorldUIMinDistOverrideV2;
         [NMS(Index = 32)]
-        /* 0x330 */ public float InWorldUIScaler;
+        /* 0x340 */ public float InWorldUIScaler;
         // size: 0x3
         public enum OverrideInteriorExteriorMarkerEnum : uint {
             No,
@@ -72,36 +74,36 @@ namespace libMBIN.NMS.GameComponents
             Exterior,
         }
         [NMS(Index = 29)]
-        /* 0x334 */ public OverrideInteriorExteriorMarkerEnum OverrideInteriorExteriorMarker;
+        /* 0x344 */ public OverrideInteriorExteriorMarkerEnum OverrideInteriorExteriorMarker;
         [NMS(Index = 5)]
-        /* 0x338 */ public GcAlienRace Race;
+        /* 0x348 */ public GcAlienRace Race;
         [NMS(Index = 24)]
-        /* 0x33C */ public float SecondaryCameraTransitionTime;
+        /* 0x34C */ public float SecondaryCameraTransitionTime;
         [NMS(Index = 19)]
-        /* 0x340 */ public GcInteractionType SecondaryInteractionType;
+        /* 0x350 */ public GcInteractionType SecondaryInteractionType;
         [NMS(Index = 39)]
-        /* 0x344 */ public bool AllowMissionUnderwater;
+        /* 0x354 */ public bool AllowMissionUnderwater;
         [NMS(Index = 15)]
-        /* 0x345 */ public bool BroadcastTriggerAction;
+        /* 0x355 */ public bool BroadcastTriggerAction;
         [NMS(Index = 18)]
-        /* 0x346 */ public bool InteractInvertFace;
+        /* 0x356 */ public bool InteractInvertFace;
         [NMS(Index = 37)]
-        /* 0x347 */ public bool InWorldUIUseCameraUp;
+        /* 0x357 */ public bool InWorldUIUseCameraUp;
         [NMS(Index = 40)]
-        /* 0x348 */ public bool OnlyAvailableInAbandonedMode;
+        /* 0x358 */ public bool OnlyAvailableInAbandonedMode;
         [NMS(Index = 8)]
-        /* 0x349 */ public bool RepeatInteraction;
+        /* 0x359 */ public bool RepeatInteraction;
         [NMS(Index = 30)]
-        /* 0x34A */ public bool ReseedAfterRewardSuccess;
+        /* 0x35A */ public bool ReseedAfterRewardSuccess;
         [NMS(Index = 7)]
-        /* 0x34B */ public bool SecondaryMeshAlwaysVisible;
+        /* 0x35B */ public bool SecondaryMeshAlwaysVisible;
         [NMS(Index = 9)]
-        /* 0x34C */ public bool UseInteractCamera;
+        /* 0x35C */ public bool UseInteractCamera;
         [NMS(Index = 2)]
-        /* 0x34D */ public bool UseIntermediateUI;
+        /* 0x35D */ public bool UseIntermediateUI;
         [NMS(Index = 31)]
-        /* 0x34E */ public bool UsePersonalPersistentBuffer;
+        /* 0x35E */ public bool UsePersonalPersistentBuffer;
         [NMS(Index = 41)]
-        /* 0x34F */ public bool UseUnlockedInteractionIfMaintDone;
+        /* 0x35F */ public bool UseUnlockedInteractionIfMaintDone;
     }
 }
