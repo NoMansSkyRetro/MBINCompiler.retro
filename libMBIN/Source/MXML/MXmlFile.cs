ed5057ff164d4a97c3ff31b33004788db7d95ec6
HolterPhylo <robertled@gmail.com>
2025-10-27 12:22:18 -0400

    Added back WriteTemplate(NMSTemplate template)

--- libMBIN/Source/MXML/MXmlFile.cs
+++ libMBIN/Source/MXML/MXmlFile.cs
@@ -92,6 +92,13 @@ namespace libMBIN
         /// <param name="outputpath">The location to write the .mxml file.</param>
         /// <param name="hideVersionInfo">If true, version info is not written to the MXML file.</param>
         /// <param name="includeTypeInfo">If true, type info is written to the MXML file.</param>
+        public static string WriteTemplate(NMSTemplate template) => WriteTemplate(template, false, false);
+        /// <summary>
+        /// Writes the NMSTemplate object to an .mxml file.
+        /// </summary>
+        /// <param name="outputpath">The location to write the .mxml file.</param>
+        /// <param name="hideVersionInfo">If true, version info is not written to the MXML file.</param>
+        /// <param name="includeTypeInfo">If true, type info is written to the MXML file.</param>
         public static string WriteTemplate(NMSTemplate template, bool hideVersionInfo, bool includeTypeInfo)
         {
             var origCulture = Thread.CurrentThread.CurrentCulture;
