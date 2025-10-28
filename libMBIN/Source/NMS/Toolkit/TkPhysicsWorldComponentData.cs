136f6bc4f5d0ba59bfb668440d500324c78e1769
HolterPhylo <robertled@gmail.com>
2025-10-28 13:05:49 -0400

    UpdateFor20576816

--- libMBIN/Source/NMS/Toolkit/TkPhysicsWorldComponentData.cs
+++ libMBIN/Source/NMS/Toolkit/TkPhysicsWorldComponentData.cs
@@ -1,9 +1,11 @@
 namespace libMBIN.NMS.Toolkit
 {
-    [NMS(GUID = 0x8707747C98418DF2, NameHash = 0xB7832FA2)]
+    [NMS(GUID = 0xFAA5A0922BA74CC2, NameHash = 0xB7832FA2)]
     public class TkPhysicsWorldComponentData : NMSTemplate
     {
         [NMS(Index = 0)]
-        /* 0x0 */ public float SafeRadius;
+        /* 0x0 */ public int MaximumNumberOfBodies;
+        [NMS(Index = 1)]
+        /* 0x4 */ public float SafeRadius;
     }
 }
