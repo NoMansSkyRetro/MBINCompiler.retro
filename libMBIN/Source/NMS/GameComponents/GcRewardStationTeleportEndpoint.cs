4b96bc9ee6fc7c3c5221b0581c9b7f084f68b160
HolterPhylo <robertled@gmail.com>
2025-11-06 15:30:16 -0500

    Update for 20696703

--- libMBIN/Source/NMS/GameComponents/GcRewardStationTeleportEndpoint.cs
+++ libMBIN/Source/NMS/GameComponents/GcRewardStationTeleportEndpoint.cs
@@ -1,7 +1,9 @@
 namespace libMBIN.NMS.GameComponents
 {
-    [NMS(GUID = 0x1AC41D7397DD48F9, NameHash = 0xF11D1A94)]
+    [NMS(GUID = 0x1DCD4FE2C5B96CD6, NameHash = 0xF11D1A94)]
     public class GcRewardStationTeleportEndpoint : NMSTemplate
     {
+        [NMS(Index = 0)]
+        /* 0x0 */ public bool FailPeekIfCannotAdd;
     }
 }
