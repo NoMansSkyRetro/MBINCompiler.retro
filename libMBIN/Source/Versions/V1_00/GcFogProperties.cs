f8f58b85d5a2346cb633dbc23350a90b2f68b62a
Qjimbo <qjimbo@gmail.com>
2026-08-31 11:29:23 -0700

    V1_00 GcSkyGlobals byte-perfect: fog IsRaining is a float on disc, plus two extra tail colour rows

---
+++ libMBIN/Source/Versions/V1_00/GcFogProperties.cs
@@ -0,0 +1,28 @@
+using libMBIN.NMS;
+using libMBIN.NMS.GameComponents;
+using libMBIN.NMS.Toolkit;
+using libMBIN.NMS.GameComponents;
+
+namespace libMBIN.V1_00.Structs
+{
+    public class GcFogProperties : NMSTemplate
+    {
+        public float FogStrength;
+        public float FogMax;
+        public float FogColourStrength;
+        public float FogColourMax;
+        public float HeightFogStrength;
+        public float HeightFogFadeOutStrength;
+        public float HeightFogOffset;
+        public float HeightFogMax;
+        public float FogHeight;
+        public GcHeavyAirSetting HeavyAirSettings;
+        public float CloudRatio;
+        public float FullscreenEffect;
+        public float DepthOfField;
+        public float DepthOfFieldDistance;
+        public float DepthOfFieldFade;
+        public float RainAmount; // disc: float, not bool
+
+            }
+}
