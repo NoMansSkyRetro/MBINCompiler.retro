e124a34f84a355ef373da34c1200091355bdcd6a
Qjimbo <qjimbo@gmail.com>
2026-08-31 04:33:32 -0700

    V1_13 GcPlanetGenerationIntermediateData: bind the 0x230 options element in planets
    The planet chain (GcPlanetData -> GenerationData) fell back to the base
    struct, whose ExternalObjectLists bound the 0x220 base
    GcExternalObjectListOptions. The 1.13 era element is 0x230 (ResourceHint
    present, stride confirmed by PLANT at +0x230 in BARRENPLANET). 1.13 goes
    178 -> 181 clean.

---
+++ libMBIN/Source/Versions/V1_13/GcPlanetGenerationIntermediateData.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+
+using libMBIN.NMS;
+using libMBIN.NMS.Toolkit;
+using libMBIN.NMS.GameComponents;
+
+namespace libMBIN.V1_13.Structs
+{
+    public class GcPlanetGenerationIntermediateData : NMSTemplate
+    {
+        public GcSeed Seed;
+        public int TerrainSettingIndex;
+        public GcGalaxyStarTypes GalaxyStarType;
+        public GcPlanetClass PlanetClass;
+        public GcPlanetSize PlanetSize;
+        public GcCreatureRoleDataTable CreatureRoleData;
+        public GcTerrainControls Terrain;
+        public GcBiomeType Biome;
+
+        public NMSString0x80 TerrainFile;
+        public NMSString0x80 CreatureLandFile;
+        public NMSString0x80 CreatureCaveFile;
+        public NMSString0x80 CreatureWaterFile;
+        public NMSString0x80 CreatureAirFile;
+        public NMSString0x80 CreatureRobotFile;
+        public List<GcExternalObjectListOptions> ExternalObjectLists;
+        public List<int> ExternalObjectListIndices;
+    }
+}
