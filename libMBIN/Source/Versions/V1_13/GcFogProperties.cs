9e26e39d828b4f0cd2ee7fb2dfa63f3463b93540
Qjimbo <qjimbo@gmail.com>
2026-08-31 06:05:42 -0700

    V1_13 GcFogProperties: IsRaining is a raw byte (era carries 0x8F): SkyGlobals byte-perfect

---
+++ libMBIN/Source/Versions/V1_13/GcFogProperties.cs
@@ -0,0 +1,30 @@
+using libMBIN.NMS;
+using libMBIN.NMS.GameComponents;
+using libMBIN.NMS.Toolkit;
+using libMBIN.NMS.GameComponents;
+
+namespace libMBIN.V1_13.Structs
+{
+    public class GcFogProperties : NMSTemplate
+    {
+        public float FogStrength;
+        public float FogMax;
+        public float FogColourStrength;
+        public float FogColourMax;
+        public float HeightFogStrength;
+        public float HeightFogFadeOutStrength;
+        public float HeightFogOffset;
+        public float HeightFogMax;
+        public float FogHeight;
+        public GcHeavyAirSetting HeavyAirSettings;
+        public float CloudRatio;
+        public float FullscreenEffect;
+        public float DepthOfField;
+        public float DepthOfFieldDistance;
+        public float DepthOfFieldFade;
+        public byte IsRaining; // era files carry 0x8F here; byte preserves it
+
+        [NMS(Size = 0x3, Ignore = true)]
+        public byte[] Padding;
+    }
+}
