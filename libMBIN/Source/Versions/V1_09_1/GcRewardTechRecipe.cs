86158a43ce9668e37acc986a204670225752e601
Qjimbo <qjimbo@gmail.com>
2026-08-31 05:34:34 -0700

    V1_09_1 GcRewardTechRecipe: RewardTable byte-perfect
    Same 0x18 shape as the later eras (dword between Category and
    RewardGroup). The base def's RewardGroup at +4 read one dword early and
    its Ignore padding swallowed the next item's PercentageChance.

---
+++ libMBIN/Source/Versions/V1_09_1/GcRewardTechRecipe.cs
@@ -0,0 +1,14 @@
+using libMBIN.NMS;
+using libMBIN.NMS.Toolkit;
+using libMBIN.NMS.GameComponents;
+
+namespace libMBIN.V1_09_1.Structs
+{
+    public class GcRewardTechRecipe : NMSTemplate // 0x18
+    {
+        public GcTechnologyCategory Category;
+        public int Unknown4;
+        [NMS(Size = 0x10)]
+        public string RewardGroup;
+    }
+}
