6dfbadd2d8b82fda3370c9598145f4cdf29a82fb
Qjimbo <qjimbo@gmail.com>
2026-08-31 01:43:55 -0700

    V1_09_1 voxel generator chain copied from V1_13 (groundwork)
    1.09.1 has ten 13080-byte voxel elements (the 1.13 shape is 13064): the
    copied chain moves the failure from the 6-element base def to a +16-per-
    element residual inside TkVoxelGeneratorData; the remaining delta still
    needs sub-struct sizing.

---
+++ libMBIN/Source/Versions/V1_09_1/TkVoxelGeneratorSettingsArray.cs
@@ -0,0 +1,8 @@
+﻿using libMBIN.NMS; using libMBIN.NMS.GameComponents; using libMBIN.NMS.Globals; using libMBIN.NMS.Toolkit; namespace libMBIN.V1_09_1.Structs
+{
+    public class TkVoxelGeneratorSettingsArray : NMSTemplate
+    {
+        [NMS(Size = 0xA)]
+        public TkVoxelGeneratorSettingsElement[] TerrainSettings;
+    }
+}
---
+++ libMBIN/Source/Versions/V1_09_1/TkVoxelGeneratorSettingsElement.cs
@@ -0,0 +1,8 @@
+﻿using libMBIN.NMS; using libMBIN.NMS.GameComponents; using libMBIN.NMS.Globals; using libMBIN.NMS.Toolkit; namespace libMBIN.V1_09_1.Structs
+{
+    public class TkVoxelGeneratorSettingsElement : NMSTemplate
+    {
+        public TkVoxelGeneratorData Min;
+        public TkVoxelGeneratorData Max;
+    }
+}
