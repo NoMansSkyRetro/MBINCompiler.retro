96d5d1cb5e56cb1bd85c98fc18eab22ddef8e552
Qjimbo <qjimbo@gmail.com>
2026-08-31 02:38:26 -0700

    V1_13 GcRealityManagerData byte-perfect (tail padding is 8, not 0xC)
    Also seeds V1_09_1 with the corrected def; its residual is a 4-byte stride
    difference inside the stats blocks around 0x47B8, still to derive.

--- libMBIN/Source/Versions/V1_09_1/GcRealityManagerData.cs
+++ libMBIN/Source/Versions/V1_09_1/GcRealityManagerData.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 
-using libMBIN.NMS; using libMBIN.NMS.GameComponents; using libMBIN.NMS.Globals; using libMBIN.NMS.Toolkit; namespace libMBIN.V1_13.Structs
+using libMBIN.NMS; using libMBIN.NMS.GameComponents; using libMBIN.NMS.Globals; using libMBIN.NMS.Toolkit; namespace libMBIN.V1_09_1.Structs
 {
     public class GcRealityManagerData : NMSTemplate
     {
